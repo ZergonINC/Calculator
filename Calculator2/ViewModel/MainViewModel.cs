c98331a14406f8d9e032631e20a5d1a61149d849
Сергей Кудря <SergayKC@mail.ru>
2022-04-22 20:30:58 +0300

    Добавьте файлы проекта.

---
+++ Calculator2/ViewModel/MainViewModel.cs
@@ -0,0 +1,166 @@
+﻿using Calculator2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator2.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        private string _display;
+
+        public string Display
+        {
+            get { return _display; }
+            set
+            {
+                _display = value;
+                RaisePropertyChanged(nameof(Display));
+            }
+        }
+
+
+        private string _secondDisplay;
+
+        public string SecondDisplay
+        {
+            get { return _secondDisplay; }
+            set
+            {
+                _secondDisplay = value;
+                RaisePropertyChanged(nameof(SecondDisplay));
+            }
+        }
+
+        #region Base commands
+        public ICommand NumberCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    Display = "0";
+                });
+            }
+        }
+
+        public ICommand ArithmeticCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    
+                });
+            }
+        }
+
+        public ICommand EqualsCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+        #endregion
+
+        #region Clear commands
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand CleanEntryCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand BackspaceCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+        #endregion
+
+        #region Memory commands
+        public ICommand MemoryClearCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand MemoryReadCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand MemorySaveCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand MemoryPlusCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand MemoryMinusCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+
+                });
+            }
+        }
+        #endregion
+    }
+}
